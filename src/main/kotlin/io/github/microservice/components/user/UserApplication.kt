2cd66f631ba1f396bb89be7d95141ff5294861c8
hookszhang <hookszhang@gmail.com>
2018-03-26 17:26:38 +0800

    Enable feign clients

--- src/main/kotlin/io/github/microservice/components/user/UserApplication.kt
+++ src/main/kotlin/io/github/microservice/components/user/UserApplication.kt
@@ -3,7 +3,9 @@ package io.github.microservice.components.user
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 class UserApplication
