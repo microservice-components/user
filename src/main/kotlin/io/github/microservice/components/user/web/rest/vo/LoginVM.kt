acd5351b4df2767fe735094012e5d6ba0c337045
hookszhang <hookszhang@gmail.com>
2018-03-29 17:52:44 +0800

    Fix kotlin and @Valid Spring annotation

--- src/main/kotlin/io/github/microservice/components/user/web/rest/vo/LoginVM.kt
+++ src/main/kotlin/io/github/microservice/components/user/web/rest/vo/LoginVM.kt
@@ -2,9 +2,9 @@ package io.github.microservice.components.user.web.rest.vo
 
 import io.github.microservice.components.user.enums.Device
 import io.github.microservice.components.user.enums.From
-import javax.validation.constraints.NotNull
+import javax.validation.constraints.NotEmpty
 
 data class LoginVM(
-        @NotNull
+        @field:NotEmpty
         var phone: String?, var from: From?, var device: Device?, var inviteCode: String?, var invitedCode: String?
 )
